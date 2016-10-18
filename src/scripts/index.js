d5b62af17e511288264a642a0e32c2cc217aff38
pander0420 <1075803802@qq.com>
2016-10-18 21:06:55 +0800

    10.18
    增加部分内容

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,9 @@
 //引入zepto
 var $ = require('./components/zepto-modules/_custom');
 
+
+//引入weixin-js-sdk
+var wx = require('./components/weixin-js-sdk/index');
  //引入iscroll
 
 var Iscroll = require('./components/iscroll/iscroll.js');
@@ -61,7 +64,7 @@ var SwiperAnimate = require('./components/swiper/swiper.animate1.0.2.min');
  $("#footer div").tap(function(){
 
  	var apiTarget = $(this).attr("id"); 
-  $("#header").html(apiTarget);
+  $("#title").html(apiTarget);
   if(apiTarget=="skill"||apiTarget=="interests"){
     $.post('/api/'+apiTarget, {}, function(response){
 
@@ -87,7 +90,6 @@ var SwiperAnimate = require('./components/swiper/swiper.animate1.0.2.min');
         html+="<li><span class='iconfont'>"+response[i].iconfont+"</span><div class='category'>"+response[i].category+"</div>"+"<div class='name'>"+response[i].name+"</div>"+"</li>";
         
       }
-
       $("#scroller ul").html(html);
     })
   }
@@ -106,6 +108,51 @@ var SwiperAnimate = require('./components/swiper/swiper.animate1.0.2.min');
     $('#preload').show();
   }
  },100);
+ 
+
+ console.log($.post());
+$("#qcode").tap(function(){
+    $.post("http://1.1075803802.applinzi.com/php/getsign.php",{
+        url:window.location.href
+    },function(data){
+        pos = data.indexOf('}');
+        dataStr = data.substring(0,pos+1);
+        objData = JSON.parse(dataStr);
+        console.log(dataStr);
+            wx.config({
+                debug:true,
+                appId:objData.appId,
+                timestamp: objData.timestamp,
+                nonceStr:objData.nonceStr,
+                signature:objData.signature,
+                jsApiList:[
+                'scanQRCode','getLocation'
+                //所有要调用的API都要加到这个列表中
+                ]
+            });
+
+            wx.ready(function(){
+                //在这里调用 API
+               
+                wx.scanQRCode({
+                    needResult:1,//默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+                    scanType:["qrCode","barCode"],//可以指定扫二维码还是一维码，默认二者都有
+                    success:function(res){
+                        var result = res.resultStr;//当needResult为 1时，扫码返回的结果
+                    }
+                });
+                // wx.getLocation({
+                //    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+                //    success: function (res) {
+                //        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+                //        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+                //        var speed = res.speed; // 速度，以米/每秒计
+                //        var accuracy = res.accuracy; // 位置精度
+                //    }
+                //});
+            });
+    });
+})
 
 
 
